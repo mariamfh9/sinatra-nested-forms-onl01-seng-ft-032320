edc4d1b82aefc4f718a412d088ea9402202aad9a
Mariam Haider <mfh.haider@gmail.com>
2020-04-30 20:33:48 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,16 +1,8 @@
 class Pirate
-  attr_accessor :name, :weight, :height 
-  
-  @@all = [] 
-  
-  def initialize(params)
-    @name = params[:name]
-    @weight = params[:weight]
-    @height = params[:height]
-    @@all << self 
-  end 
-  
-  def self.all
-    @@all
-  end 
+   attr_accessor :name, :weight, :height
+   @@all = []
+   
+   def self.all
+      @@all
+   end
 end
