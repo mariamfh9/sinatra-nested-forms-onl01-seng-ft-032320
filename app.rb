96d5d437af550705d518232a6818e1e2c66cab83
Mariam Haider <mfh.haider@gmail.com>
2020-04-30 20:51:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,23 +14,14 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new
-      @pirate.name = params[:pirate][:name]
-      @pirate.weight = params[:pirate][:weight]
-      @pirate.height = params[:pirate][:height]
+      @pirate = Pirate.new(params[:pirate])
 
-      @ship1 = Ship.new
-      @ship1.name = params[:pirate][:ships][0][:name]
-      @ship1.type = params[:pirate][:ships][0][:type]
-      @ship1.booty = params[:pirate][:ships][0][:booty]
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
 
-      @ship2 = Ship.new
-      @ship2.name = params[:pirate][:ships][1][:name]
-      @ship2.type = params[:pirate][:ships][1][:type]
-      @ship2.booty = params[:pirate][:ships][1][:booty]
-
-
-      erb :"pirates/show"
+      erb :'pirates/show'
     end
 
   end
