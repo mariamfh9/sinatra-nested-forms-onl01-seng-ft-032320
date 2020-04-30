e5f178377a268a425dfc3dafc363d01094d2e8f9
Mariam Haider <mfh.haider@gmail.com>
2020-04-30 20:33:05 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,12 +3,12 @@ class Pirate
   
   @@all = [] 
   
-  #def initialize(params)
-  #  @name = params[:name]
-  #  @weight = params[:weight]
-  #  @height = params[:height]
-  #  @@all << self 
- # end 
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self 
+  end 
   
   def self.all
     @@all
