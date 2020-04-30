366daa0fa824dc97f03ff7658ecdfad63549ac1c
Mariam Haider <mfh.haider@gmail.com>
2020-04-30 20:28:34 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  
+  @@all = [] 
+  
+  #def initialize(params)
+   # @name = param[:name]
+    #@type = param[:type]
+    #@booty = param[:booty]
+    #@@all << self 
+  #end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.clear 
+    self.all.clear
+  end 
 end
